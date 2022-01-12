1d350264369747580dd1c526f10202bb11e60bec
carolinejeunon <jeunonc@gmail.com>
2022-01-12 19:33:45 -0300

    ajustes finais

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.use(helmet());
+  //app.use(helmet());
   app.enableCors();
   const config = new DocumentBuilder()
 	.setTitle('Api')
@@ -17,6 +17,6 @@ async function bootstrap() {
 	.build();
 const document = SwaggerModule.createDocument(app, config);
 SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
